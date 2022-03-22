3c97f08d22afd0a16975f2a9d2f8b605a4a1d05d
toyo-oh <wangfeng1988@gmail.com>
2022-03-22 18:13:43 +0900

    rails version update

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
 	validates :email, {presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email must be valid" }, on:[:create, :update_email]}
 	validates :name, {presence: true, length: { maximum: 10}, on:[:create, :update_profile]}
 	validates :password, {presence: true, length: { in: 6..20 }, confirmation: true, on:[:create, :update_password]}
